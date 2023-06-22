1d106441acbf81a40bc47a20e57e9c5c1375f2d1
MobileGamer-hq <73354088+MobileGamer-hq@users.noreply.github.com>
2023-06-22 07:15:21 +0100

    new changes to packages

--- actions.py
+++ actions.py
@@ -1,6 +1,22 @@
-import subprocess
+import openai
 
-def open_app(app, ):
-    subprocess.call("calc.exe")
+def process_with_gpt(input_text):
+        # Authenticate with the OpenAI API
+        openai.api_key = "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"
 
-def respond():
+        # Set the model and parameters
+        model_engine = "text-davinci-002"
+        prompt = input_text
+        temperature = 0.5
+        max_tokens = 60
+
+        # Generate text using GPT-3
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+
+        # Return the generated text
+        return response.choices[0].text
