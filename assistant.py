e88b09caa9e4ea19779fe42d8a9b04415aa8bb1d
MobileGamer-hq <73354088+MobileGamer-hq@users.noreply.github.com>
2023-06-22 07:16:31 +0100

    new changes remove openai

--- assistant.py
+++ assistant.py
@@ -13,7 +13,6 @@ import warnings
 import speech_recognition as sr
 import random
 import wolframalpha
-import openai
 from playsound import playsound
 
 
@@ -57,27 +56,6 @@ class Assistant:
                 return "None"
         return statement
 
-
-    def process_with_gpt(input_text):
-        # Authenticate with the OpenAI API
-        openai.api_key = "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"
-
-        # Set the model and parameters
-        model_engine = "text-davinci-002"
-        prompt = input_text
-        temperature = 0.5
-        max_tokens = 60
-
-        # Generate text using GPT-3
-        response = openai.Completion.create(
-            engine=model_engine,
-            prompt=prompt,
-            temperature=temperature,
-            max_tokens=max_tokens
-        )
-
-        # Return the generated text
-        return response.choices[0].text
 
     def brain(self, statement):
         if "hello" in statement or "hi" in statement or "nebula" in statement:
